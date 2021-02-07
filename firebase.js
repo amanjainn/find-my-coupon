e7e838353ac7b403d8821270efbe1da6a76ff1dc
Aman <amangrand@gmail.com>
2021-02-07 15:22:51 +0530

    Connected firebase to app

---
+++ firebase.js
@@ -0,0 +1,11 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyDqrT1fnU97jhUtTQUp4ODrjivhrbbr2LQ",
+    authDomain: "find-my-coupon.firebaseapp.com",
+    projectId: "find-my-coupon",
+    storageBucket: "find-my-coupon.appspot.com",
+    messagingSenderId: "1054798639604",
+    appId: "1:1054798639604:web:6096f7f1dc4a4578ea09a1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
